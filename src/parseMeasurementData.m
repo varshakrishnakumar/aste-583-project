e138ae0ceb87b931332e03a595929b2ad5506b30
Varsha <varshakrishnakumarr@gmail.com>
2025-12-06 12:08:24 -0800

    clean up dynamics and parser

--- src/parseMeasurementData.m
+++ src/parseMeasurementData.m
@@ -1,41 +1,116 @@
 function meas = parseMeasurementData(filename, t0_et)
-% parseMeasurementData  Read truth measurement CSV and add ET timestamps.
+% parseMeasurementData  Read truth measurement CSV and return ET timestamps.
 %
 % Inputs:
-%   filename - CSV file name (e.g. 'ASTE583_Project_LTB_Measurements_0-6D_Truth.csv')
-%   t0_et    - detection epoch in ET (sec past J2000)
+%   filename - CSV file name
+%   t0_et    - (optional) detection epoch in ET [sec past J2000]
+%              Used only if the file time is relative to detection.
 %
 % Output:
 %   meas struct with fields:
-%       .t      - Nx1 measurement times in ET [s]
+%       .t         - Nx1 measurement times in ET [s]
 %       .stationID - Nx1 station IDs (1..4)
 %       .range     - Nx1 range [km]   (NaN if not present in file)
 %       .rr        - Nx1 range-rate [km/s]
 
+    if nargin < 2
+        t0_et = 0;  % default: assume times are already ET
+    end
+
     T = readtable(filename, 'VariableNamingRule','preserve');
+    varNames = string(T.Properties.VariableNames);
 
-    % Required fields
-    t_rel    = T.("Time (s)");       % seconds since detection
-    station  = T.("Station ID");
+    % -------- Time column (ET vs relative) -------- %
+    timeVar = "";
+    % Candidates for absolute ET
+    candAbs = ["Time (sec past J2000 - ET)", "Time_ET", "Time_ET_s"];
+    % Candidates for relative-to-detection
+    candRel = ["Time (s)", "Time_rel_s", "Time since detection (s)"];
 
-    % Range-rate column (handle small naming variations)
-    if any(strcmp(T.Properties.VariableNames, "Range Rate (km/s)"))
-        rr = T.("Range Rate (km/s)");
-    elseif any(strcmp(T.Properties.VariableNames, "Range rate (km/s)"))
-        rr = T.("Range rate (km/s)");
-    else
-        error('No range-rate column found in %s', filename);
+    for c = candAbs
+        if any(varNames == c)
+            timeVar = c;
+            isAbsET = true;
+            break;
+        end
+    end
+    if timeVar == ""
+        for c = candRel
+            if any(varNames == c)
+                timeVar = c;
+                isAbsET = false;
+                break;
+            end
+        end
+    end
+    if timeVar == ""
+        error('parseMeasurementData:NoTimeColumn', ...
+              'No recognizable time column found in %s', filename);
     end
 
-    % Optional range column
-    if any(strcmp(T.Properties.VariableNames, "Range (km)"))
-        range = T.("Range (km)");
+    t_raw = T.(timeVar);
+
+    if isAbsET
+        % File already in ET seconds past J2000
+        if t0_et ~= 0
+            warning('parseMeasurementData:IgnoringT0', ...
+                'Time column appears to be absolute ET; t0_et is ignored.');
+        end
+        t_et = t_raw;
     else
-        range = nan(size(rr));  % no range in 0-6D file
+        % File is relative to detection; need t0_et
+        if t0_et == 0
+            warning('parseMeasurementData:MissingT0', ...
+                ['Time column appears relative to detection but t0_et=0. ' ...
+                 'Make sure this is what you intend.']);
+        end
+        t_et = t0_et + t_raw;
     end
 
-    meas.t         = t0_et + t_rel;  % convert to ET
-    meas.stationID = station;
-    meas.range     = range;
-    meas.rr        = rr;
+    % -------- Station ID -------- %
+    if any(varNames == "Station ID")
+        station = T.("Station ID");
+    else
+        error('parseMeasurementData:NoStationID', ...
+              'No "Station ID" column found in %s', filename);
+    end
+
+    % -------- Range-rate column -------- %
+    rrVar = "";
+    candRR = ["Range Rate (km/s)", "Range rate (km/s)", "RangeRate_km_s"];
+    for c = candRR
+        if any(varNames == c)
+            rrVar = c;
+            break;
+        end
+    end
+    if rrVar == ""
+        error('parseMeasurementData:NoRangeRate', ...
+              'No range-rate column found in %s', filename);
+    end
+    rr = T.(rrVar);
+
+    % -------- Optional range column -------- %
+    rangeVar = "";
+    candR = ["Range (km)", "Range_km"];
+    for c = candR
+        if any(varNames == c)
+            rangeVar = c;
+            break;
+        end
+    end
+
+    if rangeVar ~= ""
+        range = T.(rangeVar);
+    else
+        range = nan(size(rr));  % no range in 0–6D file
+    end
+
+    % -------- Pack into struct and sort by time -------- %
+    [t_sorted, idx] = sort(t_et);
+
+    meas.t         = t_sorted(:);
+    meas.stationID = station(idx);
+    meas.range     = range(idx);
+    meas.rr        = rr(idx);
 end
--- src/projectDynamics.m
+++ src/projectDynamics.m
@@ -1,50 +1,111 @@
-function xdot = projectDynamics(T,X,params,sc)
-    %Position and Velocity Vecotrs/Magnitudes
-    r = X(1:3);
+function xdot = projectDynamics(t, X, params, sc)
+%PROJECTDYNAMICS Sun-centered dynamics (+ optional STM) for ASTE583 project.
+%
+% State (10x1):
+%   X(1:3)   r_sc_sun   [km]    Spacecraft position wrt Sun (ecliptic J2000)
+%   X(4:6)   v_sc_sun   [km/s]  Spacecraft velocity wrt Sun
+%   X(7)     k_srp      [-]     SRP scale factor
+%   X(8)     lat_stn4   [rad]   (constant in dynamics)
+%   X(9)     lon_stn4   [rad]   (constant in dynamics)
+%   X(10)    rr_bias    [km/s]  (constant in dynamics)
+%
+% Optional (appended):
+%   X(11:end) = Phi(:) where Phi is 10x10 STM (column-stacked).
+%
+% Time:
+%   t = ephemeris time seconds past J2000 (ET), compatible with SPICE.
+%
+% Output:
+%   xdot has the same length as X.
+
+    X = X(:); % ensure column vector
+
+    n = numel(X);
+    hasSTM = (n == 110);
+    if ~(n == 10 || hasSTM)
+        error('projectDynamics:BadStateLength', ...
+            'Expected X length 10 (state) or 110 (state+STM). Got %d.', n);
+    end
+
+    % -------------------- Unpack state -------------------- %
+    r     = X(1:3);
+    v     = X(4:6);
+    k_srp = X(7);
+
     rmag = norm(r);
-    v = X(4:6);
 
-    %STM
-    STM = reshape(X(11:end),10,10);
+    % -------------------- Ephemeris (Earth wrt Sun) -------------------- %
+    % NOTE: SPICE calls inside an ODE RHS can get slow for long propagations/
+    % Monte Carlo. If runtime becomes painful, precompute Earth ephemeris and
+    % interpolate instead.
+    xEarth = cspice_spkezr('EARTH', t, 'ECLIPJ2000', 'NONE', 'SUN'); % km, km/s
+    r_e    = xEarth(1:3);
+    r_emag = norm(r_e);
 
-    %SRP Values
-    P_SRP = params.psrp; %kN/km^2
-    k_SRP = X(7); %unitless
+    r_sc_e    = r - r_e;          % spacecraft wrt Earth (in same inertial frame)
+    r_sc_emag = norm(r_sc_e);
 
-    % mu parameters
-    mus = params.mus; %km^3/s^2
-    mue = params.mue; %km^3/s^2
-    AU = params.AU; % km^2
+    % -------------------- Accelerations (km/s^2) -------------------- %
+    mus = params.mus;
+    mue = params.mue;
 
-    % sc parameters
-    m = sc.m;
-    rho = sc.rho;
-    A = sc.A;
+    % Sun gravity
+    a_sun = -mus * r / rmag^3;
 
-    % Earth Vector
-    xEarth = cspice_spkezr('Earth',T, 'ECLIPJ2000', 'NONE','Sun');
-    r_earth = xEarth(1:3);
-    r_earthmag = norm(r_earth);
+    % Earth third-body (includes indirect term so a_earth = 0 when r = 0)
+    a_earth = -mue * ( r_sc_e / r_sc_emag^3 + r_e / r_emag^3 );
 
-    % Relative Spacecraft/Earth Vector
-    r32 = r - r_earth;
-    r32mag = norm(r32);
-    
-    % Jacobian Matrix
-    Amat = zeros(10,10); % Set to All Zeros
-    Amat(1:3,4:6) = eye(3); % Identity in Velocity Partials
-    G = -1*((mus/rmag^3)*eye(3) - ((A*P_SRP*k_SRP*(1+rho)*AU^2)/(m*rmag^3))*eye(3)... 
-        + ((3*A*P_SRP*k_SRP*(1+rho)*AU^2)/(m*rmag^5))*r*transpose(r) - ((3*mus)/rmag^5)*r*transpose(r)...
-        + (mue/r32mag^3)*eye(3) - ((3*mue)/r32mag^5)*r32*transpose(r32));
-    Amat(4:6,1:3) = G;
-    S = ((P_SRP*A*(1+rho)*AU^2)/m)*(r/rmag^3);
-    Amat(4:6,7) = S;
+    % SRP (no shadowing)
+    % params.psrp is in kN/km^2; with A in km^2 and m in kg, this yields km/s^2
+    P0 = params.psrp;       % kN/km^2 at 1 AU
+    AU = params.AU;         % km
+    Cr = 1 + sc.rho;        % unitless (simple reflectivity model)
 
-    %STM
-    STMdot = Amat*STM;
-    
-    % Dynamics
-    f = (-mus/rmag^3)*r - mue*(r32/r32mag^3 + r_earth/r_earthmag^3) + ((k_SRP*P_SRP*(1+rho)*A*AU^2)/m)*(r/rmag^3);
+    C0 = (P0 * sc.A * Cr * AU^2) / sc.m;  % km/s^2 * (AU/r)^2 handled via r/r^3
+    a_srp = k_srp * C0 * r / rmag^3;
 
-    xdot = [v;f;0;0;0;0;reshape(STMdot,100,1)];
+    a = a_sun + a_earth + a_srp;
+
+    % 10-state derivative (augmented params are constants in the dynamics)
+    xdot_state = [v;
+                  a;
+                  0; 0; 0; 0];
+
+    % -------------------- Optional STM propagation -------------------- %
+    if hasSTM
+        Phi = reshape(X(11:end), 10, 10);
+
+        I3 = eye(3);
+
+        % Helper: d/dr ( r / ||r||^3 ) = I/r^3 - 3 rr^T / r^5
+        rrT = r * r.';
+        r3  = rmag^3;
+        r5  = rmag^5;
+        D_r = (I3 / r3) - (3 * rrT / r5);
+
+        % Gradients wrt position
+        G_sun  = -mus * D_r;
+        G_srp  = (k_srp * C0) * D_r;
+
+        r32    = r_sc_e;
+        r32mag = r_sc_emag;
+        r32r32T = r32 * r32.';
+        r32_3  = r32mag^3;
+        r32_5  = r32mag^5;
+        D_r32  = (I3 / r32_3) - (3 * r32r32T / r32_5);
+        G_earth = -mue * D_r32;   % indirect term has no r-dependence
+
+        % Assemble A = df/dx (10x10)
+        A = zeros(10,10);
+        A(1:3,4:6) = I3;
+        A(4:6,1:3) = G_sun + G_earth + G_srp;
+        A(4:6,7)   = C0 * r / rmag^3; % da/dk_srp
+
+        Phi_dot = A * Phi;
+
+        xdot = [xdot_state;
+                Phi_dot(:)];
+    else
+        xdot = xdot_state;
+    end
 end
